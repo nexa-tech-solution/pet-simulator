2fc19a29f7533ed01d44b5a900f5644e1200a14d
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-01-03 11:32:40 +0700

    Feat | Logo

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -3,6 +3,56 @@ import { CloudSection } from '@/features/home/CloudSection';
 import { HeaderSection } from '@/features/home/HeaderSection';
 import PetSection from '@/features/home/PetSection';
 import { TrackingInfoSection } from '@/features/home/TrackingInfoSection';
+import { SITE_URL } from '@/utils/constants/meta.constant';
+import { Metadata } from 'next';
+
+export async function generateMetadata(props: Omit<LayoutProps<'/[locale]'>, 'children'>): Promise<Metadata> {
+  // const { locale } = await props.params;
+
+  // const tCommon = await getTranslations({
+  //   locale: locale as Locale,
+  //   namespace: 'common',
+  // });
+
+  const canonicalUrl = SITE_URL;
+
+  return {
+    title: `PetPal | Pet Simulator`,
+    description: `PetPal | A virtual pet simulation experience`,
+    keywords: ['PetPal', 'virtual pet', 'simulation'],
+    icons: {
+      icon: [
+        // { url: '/favicon.ico', sizes: '16x16', type: 'image/png' },
+        // { url: '/favicon.ico', sizes: '32x32', type: 'image/png' },
+        // { url: '/favicon.ico', sizes: '48x48', type: 'image/png' },
+        { url: '/favicon.ico', sizes: 'any' },
+      ],
+      // apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    },
+    openGraph: {
+      title: `PetPal | Pet Simulator`,
+      description: `PetPal | A virtual pet simulation experience`,
+      url: canonicalUrl,
+      siteName: 'PetPal',
+      images: [
+        {
+          url: `${SITE_URL}/images/logo.png`,
+          width: 1200,
+          height: 630,
+          alt: 'PetPal',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    alternates: {
+      canonical: canonicalUrl,
+      languages: {
+        en: `${SITE_URL}`,
+      },
+    },
+  };
+}
 
 export default async function HomePage() {
   return (
