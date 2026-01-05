76dc14357150e1f567d3ae14777a4682386eda30
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-01-05 22:24:05 +0700

    Hot fix | Fix Chat

--- src/app/[locale]/chat/page.tsx
+++ src/app/[locale]/chat/page.tsx
@@ -1,9 +1,10 @@
+'use client';
 import { ChatFooterSection } from '@/features/chat/ChatFooterSection';
 import { ChatHeaderSection } from '@/features/chat/ChatHeaderSection';
 import { ChatSection } from '@/features/chat/ChatSection';
 import { HeaderSection } from '@/features/home/HeaderSection';
 
-export default async function ChatPage() {
+export default function ChatPage() {
   return (
     <div
       className='flex flex-col min-h-screen w-full overflow-hidden transition-colors duration-1000 bg-linear-to-b from-sky-300 via-blue-100 to-white dark:from-slate-700 dark:via-slate-800 dark:to-slate-900     group-data-[sleep=true]:from-slate-900
