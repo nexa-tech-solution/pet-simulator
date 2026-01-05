bc9340e04743d81b05ecc69381ad5afb107322cc
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-01-05 22:26:10 +0700

    Hot fix | Fix Chat

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimisticClientCache: false,
+    ppr: false,
+    serverComponentsExternalPackages: [],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
