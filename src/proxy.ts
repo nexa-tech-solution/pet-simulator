6913d737c4a9500e1d236348af3e3dcb8022df5f
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-01-05 22:52:37 +0700

    Hot fix | Fix Router

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import type { NextRequest } from 'next/server';
 
-export default createMiddleware(routing);
+import { routing } from '@/i18n/routing';
+
+export function proxy(request: NextRequest) {
+  const response = createMiddleware(routing)(request);
+
+  return response;
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)'],
 };
