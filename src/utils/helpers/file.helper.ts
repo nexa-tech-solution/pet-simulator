b53772b8629392cbe250a8a9870e60cfdfe60a2d
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-04-26 16:32:57 +0700

    feat: change max size

--- src/utils/helpers/file.helper.ts
+++ src/utils/helpers/file.helper.ts
@@ -1,4 +1,4 @@
-export const MAX_IMAGE_FILE_SIZE = 2 * 1024 * 1024;
+export const MAX_IMAGE_FILE_SIZE = 5 * 1024 * 1024;
 
 export const readImageFile = (file: File, onLoad: (imageUrl: string) => void, onError: (message: string) => void) => {
   if (!['image/png', 'image/jpeg', 'image/webp', 'image/gif'].includes(file.type)) {
@@ -7,7 +7,7 @@ export const readImageFile = (file: File, onLoad: (imageUrl: string) => void, on
   }
 
   if (file.size > MAX_IMAGE_FILE_SIZE) {
-    onError('Image must be smaller than 2 MB.');
+    onError('Image must be smaller than 5 MB.');
     return;
   }
 
