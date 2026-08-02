c2f05e49769b95d00706b6e91c6ef5f39eed57e2
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-08-02 22:14:43 +0700

    feat: fix bug

--- src/features/chat/ChatFooterSection.tsx
+++ src/features/chat/ChatFooterSection.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
 import { isLightMode } from '@/store/theme.store';
 import { useAtom } from 'jotai';
+import { useTranslations } from 'next-intl';
 
 export const ChatFooterSection = () => {
   const t = useTranslations('common');
   const [lightModeAtom] = useAtom(isLightMode);
   return (
     <footer className={`p-4 text-center text-xs transition-colors duration-1000 ${lightModeAtom ? 'text-indigo-500' : 'text-gray-400'}`}>
-      <p>© 2026 PetPal AI Chat • {t('poweredBy', { technology: 'Gemini' })}</p>
+      <p>© 2026 PetPal AI Chat • {t('poweredBy', { technology: 'Nexa Team' })}</p>
     </footer>
   );
 };
